f6b15bae1ee02dbf2e4b46403255764015301caa
TaiseiIto <taisei1212@outlook.jp>
2025-08-04 00:43:21 +0900

    Refactor

--- delete.sh
+++ delete.sh
@@ -9,21 +9,25 @@ mount=$4
 rm -f $target
 
 # Delete mount.
-if mountpoint -q $mount; then sudo umount -l $mount; fi
-if [ -e $mount ]; then rm -rf $mount; fi
+if mountpoint -q $mount; then
+	sudo umount -l $mount
+fi
+if [ -e $mount ]; then
+	rm -rf $mount
+fi
 
 # Stop the docker container.
-if [ -n "$(docker ps --format {{.Names}} | grep -x $container)" ]; then
+if [ -n "$(docker ps --format {{.Names}} --filter name=^$container\$)" ]; then
 	docker stop $container
 fi
 
 # Delete the docker conatiner.
-if [ -n "$(docker ps -a --format {{.Names}} | grep -x $container)" ]; then
+if [ -n "$(docker ps -a --format {{.Names}} --filter name=^$container\$)" ]; then
 	docker rm $container
 fi
 
 # Delete the docker image.
-if [ -n "$(docker images --format {{.Repository}} | grep -x $image)" ]; then
+if [ -n "$(docker images --format {{.Repository}} $image)" ]; then
 	docker rmi $image
 fi
 
